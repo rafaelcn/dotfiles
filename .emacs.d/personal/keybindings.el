a137ae80fb7ecfac0d2f8aa72717f9488124a7cf
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-02-26 12:37:54 -0300

    Add a new keybinding
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- .emacs.d/personal/keybindings.el
+++ .emacs.d/personal/keybindings.el
@@ -7,9 +7,11 @@
 (global-set-key (kbd "M-$") 'split-window-below)
 (global-set-key (kbd "M-2") 'delete-other-windows)
 (global-set-key (kbd "M-0") 'delete-window)
+(global-set-key (kbd "M-RET") 'other-window)
 
 
 (with-eval-after-load 'company
   ;; company autocomplete trigger
   (define-key company-mode-map (kbd "<C-return>") 'company-complete))
 
+;; keybindings.el ends here
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # _.dotfiles_
-===========
 
 ![works badge](https://cdn.rawgit.com/nikku/works-on-my-machine/v0.2.0/badge.svg)
 
