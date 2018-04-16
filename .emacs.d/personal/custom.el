d0106df28ebc3ff3be4d3132ab0afc799e27fda2
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-04-16 08:41:08 -0300

    Add package installed verification and some vars
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -8,8 +8,7 @@
 (setq org-support-shift-select 1)
 ;; Wakatime mode
 (if (package-installed-p 'wakatime-mode)
-    ;;(when-system linux
-    (setq wakatime-api-key "<your-api-key>"))
+    (setq wakatime-api-key "<your api-key>"))
 
 (if (package-installed-p 'wakatime-mode)
     (global-wakatime-mode 1))
@@ -25,12 +24,60 @@
 (with-eval-after-load 'linum
   (set-face-attribute 'linum nil :height 100))
 
+;; company-irony configuration
+(if (package-installed-p 'company)
+    (eval-after-load 'company
+      '(add-to-list 'company-backends 'company-irony))
+
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc)))))
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(package-selected-packages
+   (quote
+    (lua-mode csharp-mode company-irony company-c-headers cmake-mode yaml-mode markdown-mode company wakatime-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc))))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(wakatime-cli-path "/home/ranu/.local/bin/wakatime")
+ '(wakatime-python-bin nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -38,3 +85,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; custom.el ends here
