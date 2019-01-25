cc5ebe5f40ad1f48f37209162c1c87b58c286482
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-01-25 16:34:49 -0200

    Changes the key to split a window horizontally
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- .emacs.d/personal/keybindings.el
+++ .emacs.d/personal/keybindings.el
@@ -1,10 +1,9 @@
 ;; Author: Rafael Campos Nunes
 ;; Some useful keybindings of my own
 
-
 ;; Window's shortcuts
 (global-set-key (kbd "M-4") 'split-window-right)
-(global-set-key (kbd "M-$") 'split-window-below)
+(global-set-key (kbd "M-5") 'split-window-below)
 (global-set-key (kbd "M-2") 'delete-other-windows)
 (global-set-key (kbd "M-0") 'delete-window)
 (global-set-key (kbd "M-RET") 'other-window)
