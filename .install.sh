1b9a6d524e26bf326730f44ff6eb0b52bb572505
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-01-31 16:10:56 -0200

    Enclosure most of the shell instructions into main
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- .install.sh
+++ .install.sh
@@ -2,7 +2,6 @@
 #
 # Rafael Campos Nunes <rafaelnunes@engineer.com>
 #
-#
 
 S="sudo"
 DIST=`cat /etc/issue | cut -d ' ' -f 1`
@@ -13,7 +12,8 @@ config_emacs() {
         $PM $INSTALL wget
     fi
 
-    wget http://ftp.gnu.org/gnu/emacs/emacs-26.1.tar.xz --show-progress --progress=bar
+    wget http://ftp.gnu.org/gnu/emacs/emacs-26.1.tar.xz --show-progress \
+         --progress=bar
     tar xf emacs-26.1.tar.xz
     cd emacs-26.1
     configure ./
@@ -31,49 +31,56 @@ config_emacs() {
 }
 
 
-# Package manager and some variables are set to work with the Debian derivatives
-# and will change if the system is not a Debian bases system.
-PM="apt"
-INSTALL="install -y"
+main() {
+    # Package manager and some variables are set to work with the Debian
+    # derivatives and will change if the system is not a Debian bases system.
+    PM="apt"
+    INSTALL="install -y"
 
-if [ -e /usr/bin/pacman ]; then
-    PM='pacman'
-    INSTALL='-S --noconfirm'
-fi
+    if [ -e /usr/bin/pacman ]; then
+        PM='pacman'
+        INSTALL='-S --noconfirm'
+    fi
 
-if [ ! -e /usr/bin/git ]; then
-    echo "You'll need git, let me install it for you.\n"
-    $S $PM install git
-fi
+    if [ ! -e /usr/bin/git ]; then
+        echo "You'll need git, let me install it for you.\n"
+        $S $PM install git
+    fi
 
-mkdir -p Github && cd Github
-git clone https://github.com/rafaelcn/dotfiles
+    mkdir -p Github && cd Github
+    git clone https://github.com/rafaelcn/dotfiles
 
-cp .gitconfig ~/
+    cp .gitconfig ~/
 
-printf "Your distribution is $DIST and it uses $PM as its package manager. (PRESS ENTER TO CONTINUE)\n"
-read
+    printf "Your distribution is $DIST and it uses $PM as its package manager. \
+(PRESS ENTER TO CONTINUE)\n"
 
-JEKYLL=ruby
-SDL2=libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
-DEPS=libtiff-dev libpng-dev libjpeg-dev libgtk-3-dev libpng-dev libgif-dev
-GENERAL=software-properties-common python-software-properties
-DEV_TOOLS=curl cmake glm zsh vim g++ gdb valgrind ocaml clang-6.0 llvm-6.0 llvm-6.0-dev llvm-6.0-tools \
-    python3-pip python-pip wget
+    read
 
-PACKAGES=$JEKYLL $SDL2 $DEV_TOOLS $DEPS
+    JEKYLL=ruby
+    SDL2=libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
+    DEPS=libtiff-dev libpng-dev libjpeg-dev libgtk-3-dev libpng-dev libgif-dev
+    GENERAL=software-properties-common python-software-properties
+    DEV_TOOLS=curl cmake glm zsh vim g++ gdb valgrind ocaml clang-6.0 llvm-6.0 \
+             llvm-6.0-dev llvm-6.0-tools \
+             python3-pip python-pip wget
 
-PYTHON=wakatime
+    PACKAGES=$JEKYLL $SDL2 $DEV_TOOLS $DEPS
 
-# Non portable between distributions I think because of package naming
-$S $PM $INSTALL $PACKAGES
-# Install wakatime package
-$S pip install $PYTHON
+    PYTHON_PACKAGES=wakatime
 
-read -p "Install and configure emacs? (y/n)" EMACS
+    # Non portable between distributions I think because of package naming
+    $S $PM $INSTALL $PACKAGES
+    # Install wakatime package
+    $S pip install $PYTHON_PACKAGES
 
-if [ $EMACS -eq "y"  ]; then
-    config_emacs
-fi
+    read -p "Install and configure emacs? (y/n)" EMACS
 
-echo "The system is ready and at your service..."
+    if [ $EMACS -eq "y"  ]; then
+        config_emacs
+    fi
+
+    echo "The system is ready and at your service..."
+}
+
+main
