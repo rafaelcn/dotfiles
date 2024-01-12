8f6162d68960d961ceaadefc9c49c0e84fdb8773
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-01-12 18:40:57 -0300

    Improve configuration of Go and Rust languages
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- .emacs.d/configuration/languages.el
+++ .emacs.d/configuration/languages.el
@@ -4,10 +4,10 @@
 
 ;; Rust
 
-(add-hook 'rust-mode-hook 'eglot-ensure)
-
 (use-package rustic
-  :ensure
+  :ensure t
+  :init
+  (setq rustic-lsp-client 'lsp-mode)
   :bind (:map rustic-mode-map
               ("M-j" . lsp-ui-imenu)
               ("M-?" . lsp-find-references)
@@ -19,25 +19,33 @@
               ("C-c C-c s" . lsp-rust-analyzer-status))
   :config
   ;; uncomment for less flashiness
+  (setq lsp-rust-analyzer-cargo-watch-command "clippy")
   (setq lsp-eldoc-hook nil)
   (setq lsp-enable-symbol-highlighting nil)
   (setq lsp-signature-auto-activate nil)
-
+  (setq lsp-inlay-hint-enable t)
   ;; comment to disable rustfmt on save
   (setq rustic-format-on-save t)
-  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
+
+  ;; issues with magit and non saved files
+  (add-hook 'rustic-mode-hook 'rustic-save-hook))
+
+(defun rustic-save-hook ()
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t)))
 
 ;; Go
 
-(global-set-key (kbd "C-.")
-				'godef-jump)
+(use-package go-mode
+  :ensure
+  :bind (:map go-mode-map
+			  ("C-." . 'godef-jump)
+			  ("C-," . 'godef-jump-other-window))
+  :config
+  (progn
+	(setq gofmt-command "goimports")                ; goimports imports missing deps and formats your code
+	(add-hook 'before-save-hook 'gofmt-before-save) ; it uses the gofmt-before-save command from gomode
+	(add-hook 'go-mode-hook #'lsp-deferred)))
 
-(global-set-key (kbd "C-,")
-				'godef-jump-other-window)
 
-(progn
-  (setq gofmt-command "goimports")                ; goimports imports missing deps and formats your code
-  (add-hook 'before-save-hook 'gofmt-before-save) ; it uses the gofmt-before-save command from gomode
-  (add-hook 'go-mode-hook #'lsp-deferred)
-  (add-hook 'go-mode-hook 'display-line-numbers-mode))
 
