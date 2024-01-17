d8beee2cd226342407a976843b2e2bde1bbee28b
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-01-17 15:19:02 -0300

    Change macro from setq-default to setopt
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- .emacs.d/configuration/utilities.el
+++ .emacs.d/configuration/utilities.el
@@ -6,34 +6,33 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (column-number-mode 1)
+(eldoc-mode -1)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(setq inhibit-startup-message t
-      initial-scratch-message "Type me..."
-      cursor-type 'bar)
+(setopt inhibit-startup-message t
+	initial-scratch-message ";; Type me..."
+	cursor-type 'bar
+	tab-width 4
+	auto-save-default t
+	default-frame-alist '((font . "Noto Sans Mono 10")))
 
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying t   ; don't delink hard links
-      version-control t     ; use version numbers on backups
-      delete-old-versions t ; automatically delete excess backups
-      kept-old-versions 5
-      kept-new-versions 20)
-
-(setq-default tab-width 4
-			  auto-save-default t)
-
-(setq default-frame-alist '((font . "Noto Sans Mono 10")))
+(setopt backup-directory-alist '(("." . "~/.emacs.d/backup"))
+	backup-by-copying t   ; don't delink hard links
+	version-control t     ; use version numbers on backups
+	delete-old-versions t ; automatically delete excess backups
+	kept-old-versions 5
+	kept-new-versions 20)
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (add-hook 'prog-mode-hook 'flycheck-mode)
+(add-hook 'prog-mode-hook 'eldoc-mode)
 
 ;; Smoooooooth
 
 (use-package smooth-scrolling
   :ensure
   :config
-  (progn
-	(smooth-scrolling-mode 1)
-	(setq smooth-scroll-margin 2)))
+  (smooth-scrolling-mode 1)
+  (setq-default smooth-scroll-margin 2))
 
