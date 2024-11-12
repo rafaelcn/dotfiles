727316ab85923394c05210f919afa6a593da463a
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-11-11 23:49:22 -0300

    Remove unused themes
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- .emacs.d/configuration/theme.el
+++ .emacs.d/configuration/theme.el
@@ -6,9 +6,4 @@
 (use-package exotica-theme
   :config (load-theme 'exotica t))
 
-(use-package zweilight-theme)
-(use-package twilight-theme)
-(use-package twilight-bright-theme)
-(use-package twilight-anti-bright-theme)
-
 ;; the behaviour of a theme might be controlled by what's defined within the keybindings.el file.
