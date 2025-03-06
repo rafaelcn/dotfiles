9d2107ce1cbdc49bc017f861cd6e9671bf2eb1d2
Rafael Campos Nunes <rcamposnunes@outlook.com>
2025-03-06 15:01:20 -0300

    Add GOPATH to be copied as well
    The go package doesn't have that setup and it always clutter my home
    folder so this fix it.
    
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- .emacs.d/configuration/system.el
+++ .emacs.d/configuration/system.el
@@ -1,6 +1,5 @@
 (when (eq system-type 'darwin)
-  (setq server-use-tcp nil)
   (use-package exec-path-from-shell
 	:init
 	(exec-path-from-shell-initialize)
-	(exec-path-from-shell-copy-envs '("LIBRARY_PATH" "INFOPATH" "CPATH" "MANPATH"))))
+	(exec-path-from-shell-copy-envs '("LIBRARY_PATH" "INFOPATH" "CPATH" "MANPATH" "GOPATH"))))
